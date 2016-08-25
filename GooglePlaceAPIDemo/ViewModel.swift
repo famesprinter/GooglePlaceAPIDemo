79c381b28593ef183b6c9453a238bcae6cba8b0c
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2016-08-25 20:31:47 +0700

    - Create ViewModel and import Alamofire.

---
+++ GooglePlaceAPIDemo/ViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ViewModel.swift
+//  GooglePlaceAPIDemo
+//
+//  Created by Kittitat Rodphotong on 8/25/2559 BE.
+//  Copyright © 2559 FameSprinter. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ViewModel {
+    
+    
+}
